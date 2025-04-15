3eeb75d3a7acc601403295764c3b7eea9ad7afc9
yzemlyak <yzemlyak@gmail.com>
2025-04-15 02:52:06 +0000

    added dynamic initially

--- app.rb
+++ app.rb
@@ -52,3 +52,15 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dynamic/50/6") do
+  @rolls = []
+
+  50.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
